d3b56b3623cb4316c31dad6b3a73ab879087e254
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-02-10 15:24:53 +0000

    set up procfile

--- server/server.js
+++ server/server.js
@@ -9,6 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile('index.html');
 });
 
-app.listen(3000, function () { // NEW
-  console.log('App running on port 3000');
+app.listen(process.env.PORT || 3000, function () { // NEW
+  console.log('App running');
 });
